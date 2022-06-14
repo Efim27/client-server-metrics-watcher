1e0b569861fd5156afead7b6cded8e671ce8fa79
Efim <42698670+Efim27@users.noreply.github.com>
2022-06-14 17:54:57 +0300

    Server: add StoreConfig

--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -8,6 +8,11 @@ import (
 
 type Config struct {
 	ServerAddr string `env:"ADDRESS" envDefault:"127.0.0.1:8080"` //addr:port
+	Store      struct {
+		Interval string `env:"STORE_INTERVAL" envDefault:"300"`
+		File     string `env:"STORE_FILE" envDefault:"/tmp/devops-metrics-db.json"`
+		Restore  bool   `env:"RESTORE" envDefault:"true"`
+	}
 }
 
 func LoadConfig() Config {
