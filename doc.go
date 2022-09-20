22254f009555a4e9a5d9baba4395e6f7525a9bdb
Efim <42698670+Efim27@users.noreply.github.com>
2022-09-20 14:47:32 +0300

    minor fix

--- doc.go
+++ doc.go
@@ -1,8 +1,8 @@
-// Package goDevopsMetrics - первый учебный проект на Go
+// Package godevopsmetrics - первый учебный проект на Go
 //
 // состоит из:
 // клиента для сбора и отправки runtime метрик
 // сервера HTTP (подробнее в swagger документации)
 //
 // Описание конфига можно найти в internal/.../config
-package goDevopsMetrics
+package godevopsmetrics
